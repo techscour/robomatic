2f3e7eb151c952508c6dc315effbf3b5404de284
George Maney <gmaney@ieee.org>
2014-02-22 10:40:14 -0800

    new

---
+++ parser.rb
@@ -0,0 +1,52 @@
+
+
+module Preprocessor
+
+    def parse(x)
+      x = x.strip()
+      stack = []
+      tokens = []
+      token = ''
+      chars = x.split('')
+      final = chars.pop()
+      chars.each() do |char|
+        if '[({'.include?(char)
+          tokens.push token.strip()
+          token = ''
+        end
+        token += char
+        if '])}'.include?(char)
+          tokens.push token.strip()
+          token = ''
+        end
+      end
+      token = token.strip()
+      tokens.push(token) unless token == ''
+      tokens << final
+      tokens
+    end
+
+    def package(x)
+      result = {}
+      result[:literals] = []
+      result[:variables] = []
+      result[:punctuation] = x.pop()
+      x.each() do |token|
+        first = token[0]
+        if '[({'.include?(first)
+    	result[:variables] << token
+        else
+    	result[:literals] << token
+        end
+      end
+      result
+    end
+end
+
+if false
+  include Preprocessor
+  t1 = 'omega [alpha] beta (tau) gamma {delta} rho.'
+  result = package(parse(t1))
+  puts result
+end
+
---
+++ server.rb
@@ -0,0 +1,11 @@
+require 'sinatra'
+
+get '/'do
+  "Welcome to Robomatic"
+end
+get '/command' do
+  "command"
+end
+get '/compile' do
+  "command"
+end
